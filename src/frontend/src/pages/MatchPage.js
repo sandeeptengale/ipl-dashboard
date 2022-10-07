b28f029215e020032c9d93ed3c4c687a6c68c05b
sandeeptengale <sandeep.tengale@gmail.com>
2022-10-07 23:46:27 +0530

    add react component for Match Page

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -4,10 +4,27 @@ import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const MatchPage = () => {
-    
+    const [matches, setMatches] = useState([]);
+    const { teamName, year } = useParams();
+
+    useEffect(
+        () => {
+            const fetchMatches = async () => {
+                const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+                const data = await response.json();
+                console.log(data)
+                setMatches(data)
+            };
+            fetchMatches();
+        }, []
+    )
+
   return (
     <div className="MatchPage">
      <h1>MatchPage</h1>
+     {
+        matches.map(match => <MatchDetailCard teamName = {teamName} match={match}/> )
+     }
     </div>
   );
 }
